da20742d34f0b7928ffcd2330a4f11579c390b37
Patataman <lezdshas@gmail.com>
2017-02-22 13:48:37 +0100

    init medium/04

---
+++ medium/04-ReadWrite2.cpp
@@ -0,0 +1,5 @@
+/*
+	Al parecer, también se puede hacer gestión de lectura/escritura
+	mediante otras funciones propias de C++ y sus librerias.
+	El ejemplo anterior sería válido tanto para C y C++ (las funciones).
+*/

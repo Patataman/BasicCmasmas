cbbd6427d0b0ee94db90fe26d2ecbbf82cd6e21c
Patataman <lezdshas@gmail.com>
2017-03-23 16:10:49 +0100

    mas cosas

--- medium/06-const-functions.cpp
+++ medium/06-const-functions.cpp
@@ -7,9 +7,30 @@
 
 #include <iostream>
 
+//Devolución de valores fijos.
+const char* funct1() {
+	return "Ola k ase";
+}
+
+//Paso de valores constantes.
+//Sobretodo útil para ahorrar memoria (copia)
+//y se pasa un objeto grande sólo para lectura.
+void functConst2 (int const &param) {
+	//param = 12; //Explota por intentar modificar un valor constante
+}
+
+void functNoConst2 (int &param) {
+	param = 12;
+}
+
 
 int main () {
 
+	//funct1()[0] = 'b'; //Esto explota porque la cadena es const -> fija
 
+	int var = 5;
+	std::cout << "var: " << var << "\n";
+	functNoConst2(var);
+	std::cout << "var: " << var << "\n";
     return 0;
 }
